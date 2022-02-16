f36f9bd6a5eeb820315542d979272029aef1fa86
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-16 22:04:22 +0530

    added rules to each form fields

--- script.js
+++ script.js
@@ -3,7 +3,29 @@ $(document).ready(function(){
         rules:{
             fname:{
                 required:true,
-                minlength:4
+            },
+            sname:{
+                required:true,
+            },
+            emailAddress:{
+                required:true,
+                email:true
+            },
+            password:{
+                required:true,
+                minlength:8
+            },
+            day:{
+                required:true
+            },
+            month:{
+                required:true
+            },
+            year:{
+                required:true
+            },
+            gender:{
+                required:true
             }
         }
     })
